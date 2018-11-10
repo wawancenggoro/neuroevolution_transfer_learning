cf2e6ece67f462da9e6e66d0256f401afc6ad203
Albert <albertsusanto@windowslive.com>
2018-11-10 16:47:04 +0700

    Update image file location

--- retrain.py
+++ retrain.py
@@ -5,7 +5,7 @@ import model as M
 
 # you will need to customize PATH_TO_IMAGES to where you have uncompressed
 # NIH images
-PATH_TO_IMAGES = "images/"
+PATH_TO_IMAGES = "../images/"
 WEIGHT_DECAY = 1e-4
 LEARNING_RATE = 0.01
 preds, aucs = M.train_cnn(PATH_TO_IMAGES, LEARNING_RATE, WEIGHT_DECAY)
