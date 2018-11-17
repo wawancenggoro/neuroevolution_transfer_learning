8d302d479ab5324c8bcb401952ecbdceb8ce9ad1
Albert <albertsusanto@windowslive.com>
2018-11-17 21:48:40 +0700

    add comment for drop rate

--- model.py
+++ model.py
@@ -264,8 +264,10 @@ def train_cnn(PATH_TO_IMAGES, LR, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_
     if not use_gpu:
        raise ValueError("Error, requires GPU")
 
+    print("=> Training using ",DROP_RATE," drop rate")
+
     print("=> Use ",NUM_LAYERS," layers")
-    model = densenet.densenet121(pretrained=True, num_layers=NUM_LAYERS, drop_rate= DROP_RATE)
+    model = densenet.densenet121(pretrained=True, num_layers=NUM_LAYERS, drop_rate=DROP_RATE)
     
     #freezing layers
     print("=> Freezing ",FREEZE_LAYERS," layers")
