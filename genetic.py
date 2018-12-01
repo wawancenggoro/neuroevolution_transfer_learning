5fe95503ced5b86186b4fd4dd668d54bf90d00a6
Albert <albertsusanto@windowslive.com>
2018-12-01 14:28:03 +0700

    debug

--- genetic.py
+++ genetic.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
             WEIGHT_DECAY = 1e-4
             NUM_LAYERS = int(round(self.target[0]*58/100))
             FREEZE_LAYERS = int(round(self.target[1]*NUM_LAYERS/100))
-            LEARNING_RATE = random.randint(10**-6, 0.1)
+            LEARNING_RATE = random.uniform(10**-6, 0.1)
             DROP_RATE = self.target[3]*1/100
             print("LEARNING_RATE ",LEARNING_RATE)
             print("NUM_LAYERS ",NUM_LAYERS)
