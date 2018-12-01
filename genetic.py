4f3bd55cb997928b86207442f18bc9049e6e842d
Albert <albertsusanto@windowslive.com>
2018-12-01 14:26:26 +0700

    debug

--- genetic.py
+++ genetic.py
@@ -87,92 +87,6 @@ if __name__ == "__main__":
     """
     example: Mapped guess prepared Text
     """
-    # class GuessText(GeneticFunctions):
-    #     def __init__(self, target_text,
-    #                  limit=200, size=400,
-    #                  prob_crossover=0.9, prob_mutation=0.2):
-    #         self.target = self.text2chromo(target_text)
-    #         self.counter = 0
-
-    #         self.limit = limit
-    #         self.size = size
-    #         self.prob_crossover = prob_crossover
-    #         self.prob_mutation = prob_mutation
-    #         pass
-
-    #     # GeneticFunctions interface impls
-    #     def probability_crossover(self):
-    #         return self.prob_crossover
-
-    #     def probability_mutation(self):
-    #         return self.prob_mutation
-
-    #     def initial(self):
-    #         return [self.random_chromo() for j in range(self.size)]
-
-    #     def fitness(self, chromo):
-    #         # larger is better, matched == 0
-    #         return -sum(abs(c - t) for c, t in zip(chromo, self.target))
-
-    #     def check_stop(self, fits_populations):
-    #         self.counter += 1
-    #         if self.counter % 10 == 0:
-    #             best_match = list(sorted(fits_populations))[-1][1]
-    #             fits = [f for f, ch in fits_populations]
-    #             best = max(fits)
-    #             worst = min(fits)
-    #             ave = sum(fits) / len(fits)
-    #             print(
-    #                 "[G %3d] score=(%4d, %4d, %4d): %r" %
-    #                 (self.counter, best, ave, worst,
-    #                  self.chromo2text(best_match)))
-    #             pass
-    #         return self.counter >= self.limit
-
-    #     def parents(self, fits_populations):
-    #         while True:
-    #             father = self.tournament(fits_populations)
-    #             mother = self.tournament(fits_populations)
-    #             yield (father, mother)
-    #             pass
-    #         pass
-
-    #     def crossover(self, parents):
-    #         father, mother = parents
-    #         index1 = random.randint(1, len(self.target) - 2)
-    #         index2 = random.randint(1, len(self.target) - 2)
-    #         if index1 > index2: index1, index2 = index2, index1
-    #         child1 = father[:index1] + mother[index1:index2] + father[index2:]
-    #         child2 = mother[:index1] + father[index1:index2] + mother[index2:]
-    #         return (child1, child2)
-
-    #     def mutation(self, chromosome):
-    #         index = random.randint(0, len(self.target) - 1)
-    #         vary = random.randint(-5, 5)
-    #         mutated = list(chromosome)
-    #         mutated[index] += vary
-    #         return mutated
-
-    #     # internals
-    #     def tournament(self, fits_populations):
-    #         alicef, alice = self.select_random(fits_populations)
-    #         bobf, bob = self.select_random(fits_populations)
-    #         return alice if alicef > bobf else bob
-
-    #     def select_random(self, fits_populations):
-    #         return fits_populations[random.randint(0, len(fits_populations)-1)]
-
-    #     def text2chromo(self, text):
-    #         return [ord(ch) for ch in text]
-
-    #     def chromo2text(self, chromo):
-    #         return "".join(chr(max(1, min(ch, 255))) for ch in chromo)
-
-    #     def random_chromo(self):
-    #         return [random.randint(1, 255) for i in range(len(self.target))]
-    #     pass
-
-
     class evolve(GeneticFunctions):
         def __init__(self, target_text,
                      limit=200, size=10,
@@ -202,7 +116,7 @@ if __name__ == "__main__":
             WEIGHT_DECAY = 1e-4
             NUM_LAYERS = int(round(self.target[0]*58/100))
             FREEZE_LAYERS = int(round(self.target[1]*NUM_LAYERS/100))
-            LEARNING_RATE = 0.01
+            LEARNING_RATE = random.randint(10**-6, 0.1)
             DROP_RATE = self.target[3]*1/100
             print("LEARNING_RATE ",LEARNING_RATE)
             print("NUM_LAYERS ",NUM_LAYERS)
