9407d1c8a8c67d1c6f0c3ae628253ae997eb2fa8
Albert <albertsusanto@windowslive.com>
2018-12-01 14:36:39 +0700

    debug

--- model.py
+++ model.py
@@ -291,6 +291,8 @@ def train_cnn(PATH_TO_IMAGES, LR, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_
     # put model on GPU
     model = model.cuda()
 
+    print(model)
+
     # define criterion, optimizer for training
     criterion = nn.BCELoss()
     optimizer = optim.SGD(
