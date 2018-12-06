0bf170a58f9f77d79b9c0d042726f1cec5c786f2
Albert <albertsusanto@windowslive.com>
2018-12-06 13:30:14 +0700

    debug

--- printlog.py
+++ printlog.py
@@ -1,11 +1,11 @@
 def print(MESSAGE):
-	f=open("../logs/test.txt", "a")
+	f=open("/logs/test.txt", "a+")
 	f.write(MESSAGE)
 	f.write("\n")
 	f.close()
 
 def print(MESSAGE,MESSAGE_ARG):
-	f=open("../logs/test.txt", "a")
+	f=open("/logs/test.txt", "a+")
 	f.write(MESSAGE)
 	f.write("\n")
 	f.write(MESSAGE_ARG)
