eaa33e582d25fd030744063b9bb39aad0777623d
Albert <albertsusanto@windowslive.com>
2018-12-06 13:48:53 +0700

    debug

--- printlog.py
+++ printlog.py
@@ -1,5 +1,12 @@
+import datetime
+
 def print(MESSAGE):
-	f=open("logs/test.txt", "a+")
+
+	currentDT = datetime.datetime.now()
+	filename = str(currentDT.day+"-"+currentDT.month+"-"+currentDT.year+".txt")
+
+	print(MESSAGE)
+	f=open("logs/"+filename, "a+")
 	f.write(str(MESSAGE))
 	f.write("\n")
 	f.close()
