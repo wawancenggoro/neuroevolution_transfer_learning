2e6d9510a3c682d8de74702a77512aee0576448c
Albert <albertsusanto@windowslive.com>
2018-12-15 20:41:24 +0700

    update sample_images

--- cxr_dataset.py
+++ cxr_dataset.py
@@ -18,7 +18,7 @@ class CXRDataset(Dataset):
 
         self.transform = transform
         self.path_to_images = path_to_images
-        self.df = pd.read_csv("nih_labels.csv")
+        self.df = pd.read_csv("sample_images.csv")
         self.df = self.df[self.df['fold'] == fold]
 
         if(starter_images):
