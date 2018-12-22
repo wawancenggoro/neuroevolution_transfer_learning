17ee3e23242e7cccbae58785ea8bcd9e05edff2d
Albert <albertsusanto@windowslive.com>
2018-12-22 10:42:05 +0700

    print limit freeze

--- model.py
+++ model.py
@@ -291,7 +291,7 @@ def train_cnn(PATH_TO_IMAGES, LR, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_
     limit_freeze+=(FREEZE_LAYERS * 6)
     #add freeze for all transition layer that been pass through
     limit_freeze+=(transition*3)
-
+    print(limit_freeze)
     for param in model.features.parameters():
         if i< limit_freeze:
             param.requires_grad = False
