b16e23053e1d7f12448c6b0e71651d64b64d7916
Albert <albertsusanto@windowslive.com>
2019-01-23 19:20:39 +0700

    change rule in freeze layer to 0 until used layer

--- genetic.py
+++ genetic.py
@@ -237,8 +237,9 @@ if __name__ == "__main__":
 
         def random_chromo(self):
             used_layers = random.sample(range(7, 58), 1)
-            unfreeze_layers = random.sample(range(1, 3), 1)
-            freeze_layers = used_layers[0] - unfreeze_layers[0]
+            # unfreeze_layers = random.sample(range(1, 3), 1)
+            # freeze_layers = used_layers[0] - unfreeze_layers[0]
+            freeze_layers = random.sample(range(1, used_layers), 1)
             learning_rate = random.sample(range(1, 6), 1)
             drop_rate = random.sample(range(1,9),1)
 
