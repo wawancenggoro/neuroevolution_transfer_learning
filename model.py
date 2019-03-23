7e9d5a50bd3c33063e3a9cb121d22fa6da46d65f
Albert <albertsusanto@windowslive.com>
2019-03-23 13:34:53 +0700

    debug

--- model.py
+++ model.py
@@ -375,7 +375,6 @@ def train_cnn(PATH_TO_IMAGES, LR, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_
     for x in range(64):
         # image = vutils.make_grid(weight[x], normalize=True, scale_each=True)
         # writer.add_image('Image', image, x)
-        weight[x,256, 256] = [255, 0, 0]
         img = Image.fromarray(weight[x], 'RGB')
         img.save('images/',x,'.png')
 
