8c56bf53ca5d732cb4875074853b8baef34e82fd
Albert <albertsusanto@windowslive.com>
2019-05-11 17:03:30 +0700

    change batch size to 64

--- model.py
+++ model.py
@@ -248,7 +248,7 @@ def train_cnn(PATH_TO_IMAGES, LR, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_
 
     """
     NUM_EPOCHS = NUM_OF_EPOCHS
-    BATCH_SIZE = 32
+    BATCH_SIZE = 64
     currentDT = datetime.datetime.now()
     # try:
     #     rmtree('results/')
