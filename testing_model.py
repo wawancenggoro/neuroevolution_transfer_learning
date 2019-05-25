10c5db1aa9e9484248457843927bfdf98b3a748f
Albert <albertsusanto@windowslive.com>
2019-05-25 13:40:55 +0700

    testing genetic_retrain

--- testing_model.py
+++ testing_model.py
@@ -98,7 +98,7 @@ dataloaders['val'] = torch.utils.data.DataLoader(
     num_workers=0)
 
 # load model
-checkpoint_best = torch.load('results/full_retrain/checkpoint')
+checkpoint_best = torch.load('results/genetic_retrain/checkpoint')
 model = checkpoint_best['model']
 epoch_loss = 0.02
 
