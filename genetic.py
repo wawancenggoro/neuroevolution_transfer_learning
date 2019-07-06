ba1fb88b14a4d2f7bb2902f1b841b22aed7c4d3b
Albert <albertsusanto@windowslive.com>
2019-07-06 16:35:11 +0700

    set pdb

--- genetic.py
+++ genetic.py
@@ -4,6 +4,7 @@ import model as M
 
 import printlog as printlog
 import datetime
+import pdb
 
 date = datetime.date.today()
 
@@ -37,7 +38,7 @@ class GeneticAlgorithm(object):
             parent1, parent2 = parents
             father = parent1
             mother = parent2
-
+            pdb.set_trace()
             cross = random.random() < self.genetics.probability_crossover()
             print("cross "+str(cross))
             children = self.genetics.uniform_crossover(parents) if cross else parents
