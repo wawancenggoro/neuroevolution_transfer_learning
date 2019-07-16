184320efd63a9fefc49985cfac6eb3169a7b26bb
Albert <albertsusanto@windowslive.com>
2019-07-16 23:19:41 +0700

    try [58, 0, 2, 0] 100 epoch

--- retrain.py
+++ retrain.py
@@ -7,11 +7,11 @@ import model as M
 # NIH images
 PATH_TO_IMAGES = "../images_resized/"
 WEIGHT_DECAY = 1e-4
-LEARNING_RATE = 0.1
-NUM_LAYERS = 57
-FREEZE_LAYERS = 4
-DROP_RATE = 0.1
-chromosome = [57, 4, 1, 1]
+LEARNING_RATE = 0.01
+NUM_LAYERS = 58
+FREEZE_LAYERS = 0
+DROP_RATE = 0
+chromosome = [58, 0, 2, 0]
 NUM_OF_EPOCHS = 100
 preds, aucs = M.train_cnn(PATH_TO_IMAGES, LEARNING_RATE, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_RATE, chromosome, NUM_OF_EPOCHS)
 
