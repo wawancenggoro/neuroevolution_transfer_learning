8e651dec691f6211e663fa723f942a4941395758
Albert <albertsusanto@windowslive.com>
2019-12-21 13:53:59 +0700

    update batch size to 4

--- model.py
+++ model.py
@@ -251,7 +251,7 @@ def train_cnn(PATH_TO_IMAGES, LR, WEIGHT_DECAY, NUM_LAYERS, FREEZE_LAYERS, DROP_
 
     """
     NUM_EPOCHS = NUM_OF_EPOCHS
-    BATCH_SIZE = 8
+    BATCH_SIZE = 4
     currentDT = datetime.datetime.now()
     # try:
     #     rmtree('results/')
